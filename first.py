88502e193ad9c74afaccee72ec36663784f8c2f3
ufuk-ozdek <70910355+ufuk-ozdek@users.noreply.github.com>
2020-09-12 17:47:42 +0300

    Update first.py

--- first.py
+++ first.py
@@ -1,11 +1,12 @@
 
-
+from math import sqrt
+# sqrt function is basically finds square rooth of a number 
 
 
 def prime(w):
     for q in range(2,w):
-        for b in range (2,w):
-            if q % b == 0 and q > b:
+        for b in range (2, int(sqrt(q))):
+            if q % b == 0 :
                 break
         else:
             print(q)
